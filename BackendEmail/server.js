e6ad1c682536963f43c80e3904a24720b174c720
Alectmc <alectmc@gmail.com>
2024-09-12 09:41:22 -0500

    Environment used to protect information

--- BackendEmail/server.js
+++ BackendEmail/server.js
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'wesleyprayerrequests@gmail.com',
-        pass: 'PASSHERE',
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     },
 });
 
